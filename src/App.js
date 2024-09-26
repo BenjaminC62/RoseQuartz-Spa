718bc535620dec4ffb98c36e8603c27383fdbf0a
Pikablok <benjamincornet62130@gmail.com>
2024-09-26 14:53:14 +0200

    :memo: Learn what state is

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet.js';
 import Welcome from './components/Welcome.js';
+import Mess from './components/Mess.js';
 
 function App() {
   return (
     <div className="App">
       <Greet name="Pikablok"/>
       <Welcome />
+      <Mess />
     </div>
   );
 }
