2192206e8a3eddbcb53460e92a6b6e077fad9e72
Pikablok <benjamincornet62130@gmail.com>
2024-09-29 00:39:06 +0200

    :construction: update some css, image and add button discover

--- src/components/spa/Home.js
+++ src/components/spa/Home.js
@@ -1,22 +1,35 @@
 import React from "react";
-import mainImg from "./../../images/main-spa.jpg";
+import spaImage from "./../../images/main-spa.png";
 
 function Home() {
   const data = [
     {
       name: "Place of relaxation",
-      subTitle: "",
+      subTitle: "lorem ispum dolor sit amet",
     },
   ];
 
   return (
     <main>
-      <div className="arrival-page">
-        {data.map((value) => {
-          return <span className="main-title">{value.name}</span>;
-        })}
-        <img className="main-img" src={mainImg} width="100%" height="1080px" />
-      </div>
+      {data.map((value) => {
+        return (
+          <div className="arrival-page">
+            <span className="main-title">{value.name}</span>
+            <span className="main-sub-title">{value.subTitle}</span>
+            <img
+              className="main-img"
+              src={spaImage}
+              width="100%"
+              height="1080px"
+            />
+            <input
+              className="button-discovery"
+              type="button"
+              value="discover"
+            />
+          </div>
+        );
+      })}
     </main>
   );
 }
