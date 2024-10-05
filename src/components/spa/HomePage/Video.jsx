caf110403a127b99b554bc4048ae78be795e16e1
Pikablok <benjamincornet62130@gmail.com>
2024-10-05 20:41:20 +0200

    :sparkles: #11 add the quote text and image

--- src/components/spa/HomePage/Video.jsx
+++ src/components/spa/HomePage/Video.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./../../../styles/Video.css";
+import relaxationSymbol1 from "./../../../images/relaxation-symbol-1.png";
 
 function Video() {
   return (
@@ -7,6 +8,15 @@ function Video() {
       <div className="bloc-spa-video">
         <iframe className="spa-video" width="1280" height="815" src="https://www.youtube.com/embed/p7tS77quSZI?si=ik997poJmTOqrgCw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
       </div>
+      <div className="spa-quote">
+        <img className="spa-symbol-relaxation" width="300"  height="300" src={relaxationSymbol1} alt="spa" />
+        <p className="spa-quote-text">
+          "The best way to find yourself is to lose yourself in the service of others."
+          <br />
+          <span className="spa-quote-author">- Mahatma Gandhi -</span>
+        </p>
+      </div>
+      
     </section>
   );
 }
