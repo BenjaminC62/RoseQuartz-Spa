589512ae790f8914a2bca927c88d8051884798b2
Pikablok <benjamincornet62130@gmail.com>
2024-10-05 19:55:47 +0200

    :rocket: changes somes colors, end of the placement of the video

--- src/components/spa/HomePage/Video.jsx
+++ src/components/spa/HomePage/Video.jsx
@@ -5,7 +5,7 @@ function Video() {
   return (
     <section className="spa-background-next-section">
       <div className="bloc-spa-video">
-        <iframe className="spa-video"></iframe>
+        <iframe className="spa-video" width="1280" height="815" src="https://www.youtube.com/embed/p7tS77quSZI?si=ik997poJmTOqrgCw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
       </div>
     </section>
   );
