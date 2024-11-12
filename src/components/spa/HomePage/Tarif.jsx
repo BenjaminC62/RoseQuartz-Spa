83137de620f8783a68bb1c59637b269ed6a19d9a
Pikablok <benjamincornet62130@gmail.com>
2024-11-12 23:43:26 +0100

    :up_arrow: #19 - start the tarif section

--- src/components/spa/HomePage/Tarif.jsx
+++ src/components/spa/HomePage/Tarif.jsx
@@ -3,32 +3,83 @@ import '../../../styles/Tarif.css'
 
 const Tarif = () => {
   return (
-    <div>
-        <h1>Les tarifs</h1>
-        <p>Les tarifs sont les suivants:</p>
-        <ul>
-            <li>1h de massage: 50€</li>
-            <li>1h de soin: 60€</li>
-            <li>1h de soin + 1h de massage: 100€</li>
-        </ul>
-        <p>Les tarifs sont dégressifs si vous prenez plusieurs séances.</p>
-        <div className="tarif">
+    <>
+        <h1 className='tarif-title'>Les tarifs</h1>
+        <div className='tarif-container'>
             <div>
-                <h2>1 séance</h2>
-                <p>1h de massage: 50€</p>
+                <p>Les tarifs sont les suivants:</p>
+                <ul>
+                    <li>1h de massage: 50€</li>
+                    <li>1h de soin: 60€</li>
+                    <li>1h de soin + 1h de massage: 100€</li>
+                </ul>
+                <p>Les tarifs sont dégressifs si vous prenez plusieurs séances.</p>
+                <div>
+                    <div>
+                        <h2>1 séance</h2>
+                        <p>1h de massage: 50€</p>
+                    </div>
+                    <div>
+                        <h2>3 séances</h2>
+                        <p>1h de massage: 135€</p>
+                    </div>
+                    <div>
+                        <h2>5 séances</h2>
+                        <p>1h de massage: 200€</p>
+                    </div>
+                </div>
+                <span>Les tarifs sont dégressifs si vous prenez plusieurs séances.</span>
             </div>
             <div>
-                <h2>3 séances</h2>
-                <p>1h de massage: 135€</p>
+                <p>Les tarifs sont les suivants:</p>
+                <ul>
+                    <li>1h de massage: 50€</li>
+                    <li>1h de soin: 60€</li>
+                    <li>1h de soin + 1h de massage: 100€</li>
+                </ul>
+                <p>Les tarifs sont dégressifs si vous prenez plusieurs séances.</p>
+                <div>
+                    <div>
+                        <h2>1 séance</h2>
+                        <p>1h de massage: 50€</p>
+                    </div>
+                    <div>
+                        <h2>3 séances</h2>
+                        <p>1h de massage: 135€</p>
+                    </div>
+                    <div>
+                        <h2>5 séances</h2>
+                        <p>1h de massage: 200€</p>
+                    </div>
+                </div>
+                <span>Les tarifs sont dégressifs si vous prenez plusieurs séances.</span>
             </div>
             <div>
-                <h2>5 séances</h2>
-                <p>1h de massage: 200€</p>
+                <p>Les tarifs sont les suivants:</p>
+                <ul>
+                    <li>1h de massage: 50€</li>
+                    <li>1h de soin: 60€</li>
+                    <li>1h de soin + 1h de massage: 100€</li>
+                </ul>
+                <p>Les tarifs sont dégressifs si vous prenez plusieurs séances.</p>
+                <div>
+                    <div>
+                        <h2>1 séance</h2>
+                        <p>1h de massage: 50€</p>
+                    </div>
+                    <div>
+                        <h2>3 séances</h2>
+                        <p>1h de massage: 135€</p>
+                    </div>
+                    <div>
+                        <h2>5 séances</h2>
+                        <p>1h de massage: 200€</p>
+                    </div>
+                </div>
+                <span>Les tarifs sont dégressifs si vous prenez plusieurs séances.</span>
             </div>
         </div>
-        <span>Les tarifs sont dégressifs si vous prenez plusieurs séances.</span>
-        
-    </div>
+    </>
   )
 }
 
